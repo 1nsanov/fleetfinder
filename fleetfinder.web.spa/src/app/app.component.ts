d5edd283d08d21105696aa6eaac58d91e2b2dec2
1nsanov <victorbezunov@gmail.com>
2023-04-18 10:29:27 +0300

    environments

--- fleetfinder.web.spa/src/app/app.component.ts
+++ fleetfinder.web.spa/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  baseUrl: string = environment.apiUrl;
   constructor(http: HttpClient) {
-    http.get<any>('/weatherforecast').subscribe(result => {
+    http.get<any>(this.baseUrl + 'weatherforecast').subscribe(result => {
       console.log("Success connected to server! Test Data: ", result)
     }, error => console.error(error));
   }
