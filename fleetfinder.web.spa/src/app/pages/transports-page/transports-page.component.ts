e2cfe7984db28fa6cc5252ea3ee76c2e454a2dea
1nsanov <victorbezunov@gmail.com>
2023-05-01 14:10:23 +0300

    landing

--- fleetfinder.web.spa/src/app/pages/transports-page/transports-page.component.ts
+++ fleetfinder.web.spa/src/app/pages/transports-page/transports-page.component.ts
@@ -12,7 +12,12 @@ export class TransportsPageComponent {
     new DropdownItemModel(2, "item2", "Item 2"),
     new DropdownItemModel(3, "item3", "Item 3"),
     new DropdownItemModel(4, "item4", "Item 4"),
-    new DropdownItemModel(5, "item5", "Item 5")
+    new DropdownItemModel(5, "item5", "Item 5"),
+    new DropdownItemModel(6, "item6", "Item 6"),
+    new DropdownItemModel(7, "item7", "Item 7"),
+    new DropdownItemModel(8, "item8", "Item 8"),
+    new DropdownItemModel(9, "item9", "Item 9"),
+    new DropdownItemModel(10, "item10", "Item 10")
   ]
 
   onSelect(item : DropdownItemModel<string>){
