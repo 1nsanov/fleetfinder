a8139ef513f8284c03463135a0747b03dc07e3cd
victor <moshinetsae@mosreg.ru>
2023-05-31 15:35:21 +0300

    minor changes landing

--- fleetfinder.web.spa/src/app/pages/landing-page/landing-page.component.ts
+++ fleetfinder.web.spa/src/app/pages/landing-page/landing-page.component.ts
@@ -4,6 +4,8 @@ import {NotificationService} from "../../services/notification.service";
 import {cargoItems} from "../../data/transport/cargo-items";
 import {passengerItems} from "../../data/transport/passenger-items";
 import {specialItems} from "../../data/transport/special-items";
+import {Router} from "@angular/router";
+import {namesRoute} from "../../data/names-route";
 
 @Component({
   selector: 'app-landing-page',
@@ -14,4 +16,11 @@ export class LandingPageComponent {
   cargo = cargoItems;
   passenger = passengerItems;
   special = specialItems;
+
+  constructor(private router: Router) {
+  }
+
+  routeToTransports(){
+    this.router.navigate([`/${namesRoute.TRANSPORTS}`])
+  }
 }
